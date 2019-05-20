4254b6b6dbad5a8098a3f6ddf320550e65d2c600
huxq <huxingqiang@d1ev.com>
2019-05-20 22:20:20 +0800

    增加说明

--- SwiftOrderDemo/SwiftMerge.playground/Contents.swift
+++ SwiftOrderDemo/SwiftMerge.playground/Contents.swift
@@ -2,9 +2,11 @@
 
 import UIKit
 /**
- 给定一个大小为 n 的数组，找到其中的众数。众数是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
- 你可以假设数组是非空的，并且给定的数组总是存在众数。
- https://leetcode-cn.com/problems/majority-element/
+ 给定两个有序整数数组 nums1 和 nums2，将 nums2 合并到 nums1 中，使得 num1 成为一个有序数组。
+ 说明:
+ 初始化 nums1 和 nums2 的元素数量分别为 m 和 n。
+ 你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+ https://leetcode-cn.com/problems/merge-sorted-array/
  */
 
 func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
